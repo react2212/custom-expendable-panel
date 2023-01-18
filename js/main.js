0577204d7e13f439c6372c81bf483cceea6c1676
tubi55 <tubi55@nate.com>
2023-01-18 10:33:47 +0900

    panel show, hide

--- js/main.js
+++ js/main.js
@@ -3,6 +3,21 @@ const articles = frame.querySelectorAll('article');
 const prev = document.querySelector('.prev');
 const next = document.querySelector('.next');
 const opens = frame.querySelectorAll('.open');
+const closes = frame.querySelectorAll('.close');
 
 next.addEventListener('click', () => frame.append(frame.firstElementChild));
 prev.addEventListener('click', () => frame.prepend(frame.lastElementChild));
+
+opens.forEach((open) =>
+	open.addEventListener('click', (e) => {
+		e.target.closest('article').classList.add('on');
+		for (const el of articles) !el.classList.contains('on') && el.classList.add('hide');
+	})
+);
+
+closes.forEach((close) =>
+	close.addEventListener('click', (e) => {
+		e.target.closest('article').classList.remove('on');
+		for (const el of articles) el.classList.remove('hide');
+	})
+);
